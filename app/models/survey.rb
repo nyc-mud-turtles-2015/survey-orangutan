bb4571582c2a14dcd5eaba4a885dd38e6fde6d50
Rebecca Kleinberg <rebeccakleinberg@gmail.com>
2015-10-10 16:00:33 -0400

    add route / to point to views/index

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,7 +2,7 @@ class Survey < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   has_many :questions
   has_many :responses
-  has_many :respondants, through: :responses, source: :user
+  has_many :respondants, through: :responses, source: :key => "value", user
 
   validates :author_id, :title, presence: true
 end
