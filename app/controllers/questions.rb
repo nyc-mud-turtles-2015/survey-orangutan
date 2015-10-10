92611b0872b4904066e66309459a80c10ca6e6d9
Amy Liao <amyhliao88@gmail.com>
2015-10-10 15:31:24 -0700

    complete show view for question

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,5 +1,16 @@
-get '/questions/:survey_id' do
-  @questions = Question.find_by(params[:survey_id])
-  erb :'questions/:id/index'
+get '/questions' do
+  @survey = Survey.find_by(params[:id])
+  @questions = Question.where(params[:survey_id])
+  @choice = Choice.where(params[:question_id])
+
+  erb :'questions/show'
+end
+
+get '/questions/new' do
+  if logged_in?
+    erb :'questions/new'
+  else
+    erb :'sorry'
+  end
 end
 
