de73e09455bf54a9f9d45d963d01e510a4ebc0b8
Amy Liao <amyhliao88@gmail.com>
2015-10-10 23:11:18 -0700

    Edit show page and working on new view page

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,16 +1,20 @@
-get '/questions' do
-  @survey = Survey.find_by(params[:id])
-  @questions = Question.where(params[:survey_id])
-  @choice = Choice.where(params[:question_id])
-
+get '/questions/:id' do
+  @survey = Survey.find_by(id: params[:id])
+  @question = Question.find_by(id: params[:id])
   erb :'questions/show'
 end
 
 get '/questions/new' do
-  if logged_in?
-    erb :'questions/new'
-  else
-    erb :'sorry'
-  end
+  @survey = Survey.find_by(id: params[:id])
+  erb :'questions/new'
 end
 
+post '/surveys' do
+  @question = Question.new(params[:question])
+  if @question.save
+    redirect '/surveys/#{survey.id}'
+  else
+    @question.errors.messages
+    erb :'questions/new'
+  end
+end
