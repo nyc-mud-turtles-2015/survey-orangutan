99d0a130a7a3f48e6fac23b7a5ecdcf287cf1277
walsh9 <walsh9@gmail.com>
2015-10-10 23:39:16 -0400

    Add get surveys/:id route

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -2,3 +2,8 @@ get '/surveys' do
   @surveys = Survey.order(created_at: :desc).includes(:questions)
   erb :'surveys/index'
 end
+
+get '/surveys/:id' do
+  @survey = Survey.find(params[:id])
+  erb :'surveys/show'
+end
