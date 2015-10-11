e4fb1f1db8bf3b0243372bf076b94f336139d90f
Rebecca Kleinberg <rebeccakleinberg@gmail.com>
2015-10-11 19:58:31 -0400

    login form renders online

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -2,13 +2,19 @@ get '/' do
   erb :index
 end
 
-post '/users' do
-  user = User.new(params[:user])
-  if user.save
-    puts "be cool"
-    redirect '/'
-  else
-    puts "be uncool"
-    redirect '/'
-  end
+get '/login' do
+  erb :'users/login'
 end
+
+post '/login' do
+  "hello world"
+  # user = User.find_by(params[:user][:name])
+  #   if user.save
+    puts "be cool"
+    # redirect '/'
+  # else
+  #   puts "be uncool"
+    # redirect '/'
+  # end
+end
+
