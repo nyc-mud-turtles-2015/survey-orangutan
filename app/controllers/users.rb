1784d7842ccf2ae725b6b7bcb9d22d8afd9912b5
Rebecca Kleinberg <rebeccakleinberg@gmail.com>
2015-10-11 20:19:34 -0400

    post /signup saves input to the db

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -18,3 +18,12 @@ end
 get '/signup' do
   erb :'users/signup'
 end
+
+post '/signup' do
+  user = User.new(params[:user])
+  if user.save
+    redirect '/'
+  else
+    redirect '/signup'
+  end
+end
